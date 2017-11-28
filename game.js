56ab0830a8cf276e313e77ac35facee1897f4d5a
Lebranc Convas <lebrancconvas@gmail.com>
2017-11-28 09:53:47 +0700

    สร้างเงื่อนไขให้เกมสุ่มเลข
    สร้างเงื่อนไขในการสุ่มเลข
    เลขน้อยกว่า 5 ให้ขึ้นข้อความ "ตัวเลขมีค่าน้อยกว่า 5"
    เลขเท่ากับ 5 ให้ขึ้นข้อความ "ตัวเลขมีค่าเท่ากับ 5"
    เลขมากกว่า 5 ให้ขึ้นข้อความ "ตัวเลขมีค่ามากกว่า 5"

--- game.js
+++ game.js
@@ -1,2 +1,9 @@
-var randomNumbers = Math.random();
+var randomNumbers = Math.floor(Math.random() * 9);
 document.write(randomNumbers);
+if (randomNumbers < 5) {
+    alert('ตัวเลขมีค่าน้อยกว่า 5');
+} else if (randomNumbers == 5) {
+    alert('ตัวเลขมีค่าเท่ากับ 5');
+} else {
+    alert('ตัวเลขมีค่ามากกว่า 5');
+}
