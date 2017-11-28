c0e24fe3c89a5f7a78ccce2f4228846bbcfaebc9
Lebranc Convas <lebrancconvas@gmail.com>
2017-11-28 12:02:35 +0700

    เปลี่ยนโค้ดให้สามารถ Input ได้และมีการเปลี่ยนเงื่อนไข
    แก้โค้ดให้สามารถ Input ตัวเลขเข้ามาได้ระหว่าง 0 - 100 โดยให้ใส่ข้อมูลเพื่อทายตัวเลข แล้วดูว่าผลการสุ่มที่ออกมาจะเท่ากับ มากกว่า น้อยกว่า ผลที่เราใส่เข้าไป

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
-var randomNumbers = Math.floor(Math.random() * 9);
-document.write(randomNumbers);
-if (randomNumbers < 5) {
-    alert('ตัวเลขมีค่าน้อยกว่า 5');
-} else if (randomNumbers == 5) {
-    alert('ตัวเลขมีค่าเท่ากับ 5');
+var randomNumbers = Math.floor(Math.random() * 100);
+var input = prompt('ใส่เลขของคุณสิตั้งแต่ 0-100')
+if (parseInt(input) === randomNumbers) {
+    alert('เก่งมาก! คุณทายถูก' + ' ตัวเลขคือ' + randomNumbers);
+} else if (parseInt(input) < randomNumbers) {
+    alert('ผิด! ตัวเลขที่คุณใส่มามีค่าน้อยเกินไป จริงๆแล้วมันคือ ' + randomNumbers + ' ตะหาก');
+} else if (parseInt(input) > randomNumbers) {
+    alert('ผิด! ตัวเลขที่คุณใส่มามีค่ามากเกินไป จริงๆแล้วมันคือ ' + randomNumbers + ' ตะหาก');
 } else {
-    alert('ตัวเลขมีค่ามากกว่า 5');
+    alert('ค่าที่เรารับมาเกิดการผิดพลาด กรุณารีเฟรชและใส่ค่าให้เราใหม่');
 }
